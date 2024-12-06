a6dc930fb2943425769eea261f76403eaec01ed1
Casper Zandbergen <info@casperzandbergen.nl>
2024-12-06 13:46:15 +0100

    part 2

--- 2024/06.swift
+++ 2024/06.swift
@@ -6,11 +6,38 @@ let start = Date()
 var input = try String(contentsOfFile: #file.replacingOccurrences(of: ".swift", with: ".input"))
 var result2 = 0
 
-enum Facing: Character, CaseIterable {
-    case up = "^"
-    case right = ">"
-    case down = "v"
-    case left = "<"
+let input2d: [[Character]] = input.split(separator: "\n").map { $0.map { $0 } }
+let beforeGuard = input.split(separator: "^")[0].split(separator: "\n")
+var cursor = Cursor(position: .init(x: beforeGuard.last!.count, y: beforeGuard.count - 1), facing: .up)
+var visited: Set<Cursor.Position> = [cursor.position]
+
+while let next = input2d[cursor.next.position] {
+    if next == "#" {
+        cursor.facing = cursor.facing.rotated
+    } else {
+        if visited.insert(cursor.next.position).inserted {
+            var loopedCursor = cursor
+            var loopedVisitedCorners = Set<Cursor>()
+            var loopedInput2d = input2d
+            loopedInput2d[cursor.next.position.y][cursor.next.position.x] = "O"
+            o: while let next = loopedInput2d[loopedCursor.next.position] {
+                if next == "#" || next == "O" {
+                    if !loopedVisitedCorners.insert(loopedCursor).inserted {
+                        result2 += 1
+                        break o
+                    }
+                    loopedCursor.facing = loopedCursor.facing.rotated
+                } else {
+                    loopedCursor = loopedCursor.next
+                }
+            }
+        }
+        cursor = cursor.next
+    }
+}
+
+enum Facing: String, CaseIterable {
+    case up, right, down, left
 
     var rotated: Self {
         if let index = Self.allCases.firstIndex(of: self), index + 1 < Self.allCases.endIndex {
@@ -18,6 +45,7 @@ enum Facing: Character, CaseIterable {
         }
         return Self.allCases.first!
     }
+
     var mod: (x: Int, y: Int) {
         switch self {
         case .up: (0, -1)
@@ -28,25 +56,25 @@ enum Facing: Character, CaseIterable {
     }
 }
 
-let input2d: [[Character]] = input.split(separator: "\n").map { $0.map { $0 } }
-let beforeGuard = input.split(separator: "^")[0].split(separator: "\n")
-var location = (x: beforeGuard.last!.count, y: beforeGuard.count - 1)
-var facing = Facing.up
-var visited: [String: Bool] = [String(describing: location): true]
+struct Cursor: Hashable {
+    struct Position: Hashable {
+        var x: Int
+        var y: Int
+    }
+    var position: Position
+    var facing: Facing
 
-while let next = input2d[location.x + facing.mod.x, location.y + facing.mod.y] {
-    if next == "#" {
-        facing = facing.rotated
-    } else {
-        location.x += facing.mod.x
-        location.y += facing.mod.y
-        visited[String(describing: location)] = true
+    var next: Self {
+        var copy = self
+        copy.position.x = copy.position.x + facing.mod.x
+        copy.position.y = copy.position.y + facing.mod.y
+        return copy
     }
 }
 
 extension Array where Element == [Character] {
-    subscript(x: Index, y: Index) -> Character? {
-        self.indices.contains(y) && self[y].indices.contains(x) ? self[y][x] : nil
+    subscript(position: Cursor.Position) -> Character? {
+        self.indices.contains(position.y) && self[position.y].indices.contains(position.x) ? self[position.y][position.x] : nil
     }
 }
 
