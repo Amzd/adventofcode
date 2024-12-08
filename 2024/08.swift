5293332b312bd4f62a42bf668e6a3c8a813920b4
Casper Zandbergen <info@casperzandbergen.nl>
2024-12-08 14:17:14 +0100

    day 8 part 2

--- 2024/08.swift
+++ 2024/08.swift
@@ -3,47 +3,60 @@
 import Foundation
 
 let start = Date()
-var input = try String(contentsOfFile: #file.replacingOccurrences(of: ".swift", with: ".input"))
-var result2 = 0
+let input = try String(contentsOfFile: #file.replacingOccurrences(of: ".swift", with: ".input"))
 
-struct XY: Hashable {
-    let x, y: Int
-
-    static func + (lhs: Self, rhs: Self) -> Self {
-        XY(x: lhs.x + rhs.x, y: lhs.y + rhs.y)
-    }
-    static func - (lhs: Self, rhs: Self) -> Self {
-        XY(x: lhs.x - rhs.x, y: lhs.y - rhs.y)
-    }
-}
-var antennas: [Character: Set<XY>] = [:]
+// MARK: - Parse
+var antennas: [Character: Set<Location>] = [:]
 var maxX = 0
 var maxY = 0
 
 for (y, line) in input.split(separator: "\n").enumerated() {
     for (x, antenna) in line.enumerated() where antenna != "." {
-        antennas[antenna, default: []].insert(XY(x: x, y: y))
+        antennas[antenna, default: []].insert(Location(x: x, y: y))
     }
     maxX = line.count - 1
     maxY = y
 }
 
-var antinodes: Set<XY> = []
+// MARK: - Calculate antinodes
+var antinodes: Set<Location> = []
+var reasonantAntinodes: Set<Location> = []
 
 for locations in antennas.values {
     var otherLocations = locations
     while let location = otherLocations.popFirst() {
         for otherLocation in otherLocations {
             let mod = location - otherLocation
+            assert(mod.x != 0 && mod.y != 0, "stride doesn't work with 0")
+            for (x,y) in zip(stride(from: location.x, in: 0...maxX, by: mod.x), stride(from: location.y, in: 0...maxY, by: mod.y)) {
+                reasonantAntinodes.insert(Location(x: x, y: y))
+            }
+            for (x,y) in zip(stride(from: location.x, in: 0...maxX, by: -mod.x), stride(from: location.y, in: 0...maxY, by: -mod.y)) {
+                reasonantAntinodes.insert(Location(x: x, y: y))
+            }
             antinodes.insert(location + mod)
             antinodes.insert(otherLocation - mod)
         }
     }
 }
-antinodes = antinodes.filter {
-    0...maxX ~= $0.x && 0...maxY ~= $0.y
+antinodes = antinodes.filter { 0...maxX ~= $0.x && 0...maxY ~= $0.y }
+
+// MARK: - Helpers
+func stride(from: Int, in range: ClosedRange<Int>, by: Int) -> StrideThrough<Int> {
+    stride(from: from, through: by > 0 ? range.last! : range.first!, by: by)
+}
+
+struct Location: Hashable {
+    let x, y: Int
+
+    static func + (lhs: Self, rhs: Self) -> Self {
+        Location(x: lhs.x + rhs.x, y: lhs.y + rhs.y)
+    }
+    static func - (lhs: Self, rhs: Self) -> Self {
+        Location(x: lhs.x - rhs.x, y: lhs.y - rhs.y)
+    }
 }
 
 print("part1", antinodes.count)
-print("part2", result2)
+print("part2", reasonantAntinodes.count)
 print("elapsed time in seconds:", -start.timeIntervalSinceNow)
