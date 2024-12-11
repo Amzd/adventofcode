cd3fcb0db6ed435fd48989aab21d78cf19775ec8
Casper Zandbergen <info@casperzandbergen.nl>
2024-12-11 12:29:59 +0100

    day 11 part 2

--- 2024/11.swift
+++ 2024/11.swift
@@ -3,55 +3,57 @@
 import Foundation
 
 let start = Date()
-var input = try String(contentsOfFile: #file.replacingOccurrences(of: ".swift", with: ".input"))
-// input = "125 17"
-var result1 = 0
-var result2 = 0
+let input = try String(contentsOfFile: #file.replacingOccurrences(of: ".swift", with: ".input"))
 
-var row = input.split(separator: "\n")[0].split(separator: " ").map { Double($0)! }
+var numberCounts: [Double: Int] = [:]
+for number in input.split(separator: "\n")[0].split(separator: " ").map({ Double($0)! }) {
+    numberCounts[number, default: 0] += 1
+}
+
+
 for _ in 0..<25 {
-    row = row.flatMap(blink)
+    var newCounts: [Double: Int] = [:]
+    for (number, count) in numberCounts where count > 0 {
+        for result in blink(number) {
+            newCounts[result, default: 0] += count
+        }
+    }
+    numberCounts = newCounts
 }
-result1 = row.count
-for i in 25..<75 {
-    print(i, -start.timeIntervalSinceNow)
-    row = row.flatMap(blink)
+let result1 = numberCounts.values.reduce(0, +)
+for _ in 25..<75 {
+    var newCounts: [Double: Int] = [:]
+    for (number, count) in numberCounts where count > 0 {
+        for result in blink(number) {
+            newCounts[result, default: 0] += count
+        }
+    }
+    numberCounts = newCounts
 }
-result2 = row.count
+let result2 = numberCounts.values.reduce(0, +)
 
 func blink(_ number: Double) -> [Double] {
     if number == 0 {
         return [1]
     }
     let numOfDigits = numberOfDigits(of: number)
-    // print(number, number, numOfDigits)
     if numOfDigits.truncatingRemainder(dividingBy: 2) == 0 {
         return split(number, numOfDigits: numOfDigits)
     }
     return [number * 2024]
 }
 
-func split(_ number: Double, numOfDigits: Double? = nil) -> [Double] {
-    let numOfDigits = numOfDigits ?? numberOfDigits(of: number)
+func split(_ number: Double, numOfDigits: Double) -> [Double] {
     assert(numOfDigits.truncatingRemainder(dividingBy: 2) == 0)
     let lhs = floor(number / pow(10, numOfDigits/2))
     let rhs = number - lhs * pow(10, numOfDigits/2)
     return [lhs, rhs]
 }
 
-// func numberOfDigits(of number: Double) -> Double {
-//     floor(log10(number)+1)
-// }
 func numberOfDigits(of number: Double) -> Double {
-    var count = 0.0
-    var n = number
-    while n >= 1 {
-        n /= 10
-        count += 1
-    }
-    return count
+    floor(log10(number)+1)
 }
 
-print("part1", result1)
-print("part2", result2)
+print("part1", result1, result1 == 200446)
+print("part2", result2, result2 == 238317474993392)
 print("elapsed time in seconds:", -start.timeIntervalSinceNow)
