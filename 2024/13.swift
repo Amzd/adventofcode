ecaed9ee3f22bd5d0701f493d340aafcb6547047
Casper Zandbergen <info@casperzandbergen.nl>
2024-12-14 12:45:44 +0100

    day 13 cleanup

--- 2024/13.swift
+++ 2024/13.swift
@@ -29,55 +29,17 @@ while !scanner.isAtEnd {
         let a = (prizeY * bX - prizeX * bY) / (aY * bX - aX * bY)
         let b = (prizeX - a * aX) / bX
 
-        return prizeX == b * bX + a * aX
-            && prizeY == b * bY + a * aY
-            ? a * 3 + b
-            : 0;
+        if prizeX == b * bX + a * aX && prizeY == b * bY + a * aY {
+            return a * 3 + b
+        } else {
+            return 0
+        }
     }
 
     result1 += minPossible(prizeX, prizeY)
     result2 += minPossible(prizeX + 10000000000000, prizeY + 10000000000000)
-    // let correctedPrizeX = prizeX //+ 10000000000000
-    // let correctedPrizeY = prizeY //+ 10000000000000
-    // let (quotientX, _) = correctedPrizeX.quotientAndRemainder(dividingBy: bX) // + aX
-    // let (quotientY, _) = correctedPrizeY.quotientAndRemainder(dividingBy: bY)
-    // let bQuotient = min(quotientX, quotientY)
-    // let remainderX = correctedPrizeX - bX * bQuotient
-    // let remainderY = correctedPrizeY - bY * bQuotient
-    // // print("prize", prizeX, prizeY, bQuotient, remainderX, remainderY)
-    // for pressA in 0...100000000 {
-    //     guard aX * pressA < correctedPrizeX, aY * pressA < correctedPrizeY else { break }
-    //     // print("press", pressA, remainderX + bX * pressA, (remainderX + bX * pressA).quotientAndRemainder(dividingBy: aX))
-    //     if case let (aQuotientX, 0) = (remainderX + bX * pressA).quotientAndRemainder(dividingBy: aX),
-    //        case let (aQuotientY, 0) = (remainderY + bY * pressA).quotientAndRemainder(dividingBy: aY),
-    //        aQuotientX == aQuotientY {
-    //         result2 += aQuotientX * 3 + bQuotient - pressA
-    //         break
-    //     }
-    // }
-    // break
-}
-
-
-/// Returns the Greatest Common Divisor of two numbers.
-func gcd(_ x: Int, _ y: Int, search: Int = 0) -> Int {
-    var a = 0
-    var b = max(x, y)
-    var r = min(x, y)
-
-    while r > search {
-        a = b
-        b = r
-        r = a % b
-    }
-    return b
-}
-
-/// Returns the least common multiple of two numbers.
-func lcm(_ x: Int, _ y: Int) -> Int {
-    return x / gcd(x, y) * y
 }
 
 print("part1", result1, result1 == 29023)
-print("part2", result2, result2 == 0)
+print("part2", result2, result2 == 96787395375634)
 print("elapsed time in seconds:", -start.timeIntervalSinceNow)
