c6b648db120e9b9abd638fded630c8f7c671231f
Casper Zandbergen <info@casperzandbergen.nl>
2024-12-15 12:48:51 +0100

    day 15 part 2

--- 2024/15.swift
+++ 2024/15.swift
@@ -4,50 +4,85 @@ import Foundation
 
 let start = Date()
 let input = try String(contentsOfFile: #file.replacingOccurrences(of: ".swift", with: ".input"))
-var result2 = 0
 
 let inputParts = input.split(separator: "\n\n")
 let inputMapLines = inputParts[0].split(separator: "\n")
 let inputDirections = inputParts[1].replacingOccurrences(of: "\n", with: "")
-var map: [XY: Tile] = Dictionary(uniqueKeysWithValues: inputMapLines.enumerated().flatMap { (y, line) in
-    line.enumerated().compactMap { x, char in
-        Tile(rawValue: char).flatMap { (XY(x: x, y: y), $0) }
-    }
-})
-let robotLocationYX = inputMapLines.joined().split(separator: "@")[0].count.quotientAndRemainder(dividingBy: inputMapLines[0].count)
-var robotLocation = XY(x: robotLocationYX.1, y: robotLocationYX.0)
 
-moveLoop: for direction in inputDirections {
-    var boxesToPush: [XY] = []
-    var checkLocation = robotLocation.next(direction)
-    while let next = map[checkLocation] {
-        switch next {
-        case .box: boxesToPush.insert(checkLocation, at: 0)
-        case .wall: continue moveLoop
+func solve(_ inputMapLines: [Substring]) -> Int {
+    var map: [XY: Tile] = Dictionary(uniqueKeysWithValues: inputMapLines.enumerated().flatMap { y, line in
+        line.enumerated().compactMap { x, char in
+            Tile(rawValue: char).flatMap { (XY(x: x, y: y), $0) }
         }
-        checkLocation = checkLocation.next(direction)
-    }
-    if !boxesToPush.isEmpty {
-        for (newLocation, oldLocation) in zip([checkLocation] + boxesToPush.dropLast(), boxesToPush) {
-            map[newLocation] = map.removeValue(forKey: oldLocation)!
+    })
+    let robotLocationCount = inputMapLines.joined().split(separator: "@")[0].count
+    var robotLocation = XY(x: robotLocationCount % inputMapLines[0].count, y: robotLocationCount / inputMapLines[0].count)
+
+    moveLoop: for direction in inputDirections {
+        var boxesToPush: Set<XY> = []
+        var checkLocations = [robotLocation.next(direction)]
+        while let checkLocation = checkLocations.popLast() {
+            let next = map[checkLocation]
+            switch next {
+            case .box:
+                boxesToPush.insert(checkLocation)
+                checkLocations.insert(checkLocation.next(direction), at: 0)
+            case .boxLhs, .boxRhs:
+                let otherHalf: Character = next == .boxLhs ? ">" : "<"
+                boxesToPush.formUnion([checkLocation, checkLocation.next(otherHalf)])
+
+                switch direction {
+                case "^", "v":
+                    checkLocations.insert(checkLocation.next(direction), at: 0)
+                    checkLocations.insert(checkLocation.next(direction).next(otherHalf), at: 0)
+                case ">" where next == .boxRhs, "<" where next == .boxLhs:
+                    checkLocations.insert(checkLocation.next(direction), at: 0)
+                case ">", "<": // skip next because it is part of the same box
+                    checkLocations.insert(checkLocation.next(direction).next(direction), at: 0)
+                default:
+                    fatalError()
+                }
+            case .wall: continue moveLoop
+            case .none: continue
+            }
         }
+
+        for (location, tile) in boxesToPush.map({ ($0.next(direction), map.removeValue(forKey: $0)!) }) {
+            map[location] = tile
+        }
+        robotLocation = robotLocation.next(direction)
     }
-    robotLocation = robotLocation.next(direction)
+
+    return map.compactMap { location, tile in
+        switch tile {
+        case .box: location.x + location.y * 100
+        case .boxLhs: location.x + location.y * 100
+        case .boxRhs, .wall: nil
+        }
+    }.reduce(0, +)
 }
 
-let result1 = map.compactMap { location, tile in
-    switch tile {
-    case .box: location.x + location.y * 100
-    case .wall: nil
-    }
-}.reduce(0, +)
+let result1 = solve(inputMapLines)
+let result2 = solve(inputMapLines.map { line in
+    Substring(line.map { char in
+        switch char {
+        case "#": "##"
+        case ".": ".."
+        case "O": "[]"
+        case "@": "@."
+        default: fatalError()
+        }
+    }.joined())
+})
 
 enum Tile: Character {
     case wall = "#"
     case box = "O"
+    case boxLhs = "["
+    case boxRhs = "]"
 }
 
-struct XY: Hashable {
+struct XY: Hashable, CustomDebugStringConvertible {
     let x, y: Int
 
     func next(_ direction: Character) -> Self {
@@ -59,8 +94,11 @@ struct XY: Hashable {
         default: fatalError()
         }
     }
+    var debugDescription: String {
+        "(x: \(x), y: \(y))"
+    }
 }
 
 print("part1", result1, result1 == 1457740)
-print("part2", result2, result2 == 0)
+print("part2", result2, result2 == 1467145)
 print("elapsed time in seconds:", -start.timeIntervalSinceNow)
